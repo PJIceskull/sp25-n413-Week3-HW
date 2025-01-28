b85f85c28d58adf196f8d281d7f60bed019919f0
Pierce Issah <PJIceskull2003@gmail.com>
2025-01-27 20:23:51 -0500

    commit files to project repo

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Week 2 Assignment: Wordpress Themes
+
+## Week 2 Recap
+
+Blah Blah
+
+- details
+
+```php
+wp_head();
+```
---
+++ wp-content/themes/green-machine/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Green Machine</title>
+    <?php wp_head() ?>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Colors</li>
+            <li>Products</li>
+        </ul>
+    </nav>
+
+    <h1>The Green Machine</h1>
+
+</body>
+
+</html>
